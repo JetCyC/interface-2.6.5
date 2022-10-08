76964e132c93f7792a3f460889edb072019677a0
JetCyC <670182161@qq.com>
2022-10-09 00:17:01 +0800

    contract

--- src/data/Allowances.ts
+++ src/data/Allowances.ts
@@ -1,4 +1,4 @@
-import { Token, TokenAmount } from '@uniswap/sdk'
+import { Token, TokenAmount } from 'cbpinterface-sdk'
 import { useMemo } from 'react'
 
 import { useTokenContract } from '../hooks/useContract'
